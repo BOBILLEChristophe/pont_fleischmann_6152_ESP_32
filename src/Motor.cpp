618d385f25d4e493ffaf13d0f162d042d31b859f
BOBILLEChristophe <38000184+BOBILLEChristophe@users.noreply.github.com>
2019-10-01 18:12:45 +0200

    Update Motor.cpp

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -1,23 +1,27 @@
+/*
+   Motor.cpp
 
+   Ce code sert au pilotage du moteur, marche, direction, arrêt
+
+*/
 
 
 
 #include "Motor.h"
 
 // Constructeur
-Motor::Motor(uint8_t const PinA_In1, uint8_t const PinA_In2, uint8_t const PinB_In1) {
-  this->PinA_In1 = PinA_In1;
-  this->PinA_In2 = PinA_In2;
-  this->PinB_In1 = PinB_In1;
+Motor::Motor(uint8_t const pinA_In1, uint8_t const pinA_In2, uint8_t const pinB_In1) {
+  this->pinA_In1 = pinA_In1;
+  this->pinA_In2 = pinA_In2;
+  this->pinB_In1 = pinB_In1;
 
-  pinMode(PinA_In1, OUTPUT);
-  pinMode(PinA_In2, OUTPUT);
-  pinMode(PinB_In1, OUTPUT);
+  pinMode(pinA_In1, OUTPUT);
+  pinMode(pinA_In2, OUTPUT);
+  pinMode(pinB_In1, OUTPUT);
 
-  //digitalWrite(PinA_Pwm, LOW);
-  digitalWrite(PinA_In1, LOW);
-  digitalWrite(PinA_In2, HIGH);
-  digitalWrite(PinB_In1, HIGH);
+  digitalWrite(pinA_In1, LOW);
+  digitalWrite(pinA_In2, HIGH);
+  digitalWrite(pinB_In1, HIGH);
 }
 
 void Motor::drive(bool dir) {
@@ -28,17 +32,17 @@ void Motor::drive(bool dir) {
 }
 
 void Motor::brake() {
-  digitalWrite(PinB_In1, HIGH);
+  digitalWrite(pinB_In1, HIGH);
 }
 
 void Motor::fwd() {
-  digitalWrite(PinA_In1, HIGH);
-  digitalWrite(PinA_In2, HIGH);
-  digitalWrite(PinB_In1, LOW);
+  digitalWrite(pinA_In1, HIGH);
+  digitalWrite(pinA_In2, HIGH);
+  digitalWrite(pinB_In1, LOW);
 }
 
 void Motor::rev() {
-  digitalWrite(PinA_In1, LOW);
-  digitalWrite(PinA_In2, LOW);
-  digitalWrite(PinB_In1, LOW);
+  digitalWrite(pinA_In1, LOW);
+  digitalWrite(pinA_In2, LOW);
+  digitalWrite(pinB_In1, LOW);
 }
